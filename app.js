486f0cd1a79325bf841a1ce76eee3fd1bbaad9e6
Eduardo Freiberger <eduardo.silva.freiberger@gmail.com>
2025-02-20 03:25:13 -0300

    iniciando projeto

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 let amigos = []
+
+function adicionar() {
+    let nome = document.getElementById('nome-amigo');
+    let listaNomes = document.getElementById('lista-amigos');
+    listaAmigos.push(nome.value);
+
+    if (listaNomes.textContent === '' || listaNomes.textContent === null) {
+        listaNomes.textContent = nome.value;
+    } else {
+        listaNomes.textContent = listaNomes.textContent + ', ' + nome.value;
+    }
+
+    
+    nome.value = '';
+    atualizarSorteio()
+    atualizarLista();
+} 
