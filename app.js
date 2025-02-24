8f4a6244b28f0d09e528aeed0793d62273a9dd99
Eduardo Freiberger <eduardo.silva.freiberger@gmail.com>
2025-02-24 02:42:47 -0300

    adicionando nome e prompt de erro

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 let amigosSecretos = [];
 
+
+
+function adicionarAmigo(){
+
+    
+    let digito = document.getElementById("amigo");
+    let nomeDoAmigoSecreto = digito.value.trim();
+    
+    
+    if (nomeDoAmigoSecreto == '') {
+        alert("Por favor, digite um nome válido!");
+        return;
+    }
+
+    
+    amigosSecretos.push(nomeDoAmigoSecreto);
+
+    digito.value = '';
+
+    limparMenu();
+}
+
+
+
